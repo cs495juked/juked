53e6fc5667fd5496e052ea2ac41cd65e75aeadec
Francisco <frovleo1995@gmail.com>
2018-04-01 16:01:17 -0500

    Deleted createLobby.XML (also?)

--- app/src/main/java/juked/juked/CreateLobby.java
+++ app/src/main/java/juked/juked/CreateLobby.java
@@ -8,6 +8,6 @@ public class CreateLobby extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_lobby);
+        //setContentView(R.layout.activity_create_lobby);
     }
 }
