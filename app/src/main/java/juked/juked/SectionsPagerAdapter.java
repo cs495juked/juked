dc81249ffad2c7b82949dcb4553db2f9996ce48e
lauraphillips <lmphillips.8@gmail.com>
2018-04-03 14:43:20 -0500

    fixed string merge errors

--- app/src/main/java/juked/juked/SectionsPagerAdapter.java
+++ app/src/main/java/juked/juked/SectionsPagerAdapter.java
@@ -17,8 +17,8 @@ import java.util.List;
  */
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-    private final List<Fragment> mFragmentList = new ArrayList<>():
-    private final List<String> mFragmentTitleList = new ArrayList<>():
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
 
 
     public SectionsPagerAdapter(FragmentManager fm) {
